db9ddd7e3e927fb8b2e69524e5d5177105d213e9
Md Tahmid Hasan Fuad <50210878+ffuad@users.noreply.github.com>
2021-12-03 11:18:36 +0600

    Add files via upload

---
+++ Karzokrom/Karzokrom/MyKarzokrom.swift
@@ -0,0 +1,52 @@
+//
+//  MyKarzokrom.swift
+//  Karzokrom
+//
+//  Created by CSE on 11/1/21.
+//  Copyright © 2021 CSE. All rights reserved.
+//
+
+import UIKit
+
+class MyKarzokrom: UIViewController {
+
+    
+    var mystring = ""
+    
+
+    @IBOutlet weak var mylabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mylabel.text = mystring
+        //print(mystring)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func mylogoutntn(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Login")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    
+    @IBAction func myhomentn(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "home")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    @IBAction func myaddntn(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Post")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+}
